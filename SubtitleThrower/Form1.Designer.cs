50a36ce5ac6c679aaa8e64e6ca0aca212f2d5fed
Timothy Law <timlyg@yahoo.com>
2018-02-27 17:28:00 -0500

    First Batch

---
+++ SubtitleThrower/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace SubtitleThrower
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 62);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1155, 314);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(1183, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 24);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Next (F4)";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(1183, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Previous";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(1183, 353);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Copy/Exp.";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(1180, 256);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(86, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Send-Enter (F6)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(1183, 131);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Include Enter";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(238, 35);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Target Window(default is aegisub32):";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 399);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(1155, 10);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1180, 395);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Ratio";
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(1183, 172);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(86, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "FastCut (F5)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(413, 33);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(151, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Remember Position";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(1183, 201);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 12;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(1199, 197);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(67, 21);
+            this.button7.TabIndex = 13;
+            this.button7.Text = "Send ^s (F7)";
+            this.button7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1276, 411);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Subtitle Thrower";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ SubtitleThrower/Form1.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+
+namespace SubtitleThrower
+{
+    public partial class Form1 : Form
+    {
+        // global variables
+        public int prevstartread, startread, prevendread, endread;
+        public static string stopset = "，。？！」”、：；.,:;?!]\"";
+        public string processname = "aegisub32";  // set default value for processname
+        public string richtextboxtext;
+        public string file_sel_pos = "AutoTextHighlighterSelectionStart.txt",
+                      file_text = "AutoTextHighlighterSave.txt";
+
+        const int MYACTION_HOTKEY_F4 = 4;
+        const int MYACTION_HOTKEY_F5 = 5;
+        const int MYACTION_HOTKEY_F6 = 6;
+        const int MYACTION_HOTKEY_F7 = 7;
+
+        // DLL libraries used to manage hotkeys
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+
+
+        // FindWindowEx & SendMessage method. Not being used for now. Maybe in the future
+        [DllImport("user32.dll", EntryPoint = "FindWindowEx")]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, System.Text.StringBuilder text);
+
+        // Use SetForegroundWindow & SendKeys instead of FindWindowEx & SendMessage
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Get all current running processes into Combo Box1
+            comboBox1.Items.Clear();
+            Process[] MyProcess = Process.GetProcesses();
+            for (int i = 0; i < MyProcess.Length; i++)
+                comboBox1.Items.Add(MyProcess[i].ProcessName);
+            // comboBox1.Items.Add(MyProcess[i].ProcessName + "-" + MyProcess[i].Id);
+            comboBox1.Sorted = true;
+
+
+            // Modifier keys codes: Alt = 1, Ctrl = 2, Shift = 4, Win = 8
+            // Compute the addition of each combination of the keys you want to be pressed
+            // ALT+CTRL = 1 + 2 = 3 , CTRL+SHIFT = 2 + 4 = 6...
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_F4, 0, (int)Keys.F4);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_F5, 0, (int)Keys.F5);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_F6, 0, (int)Keys.F6);
+            RegisterHotKey(this.Handle, MYACTION_HOTKEY_F7, 0, (int)Keys.F7);
+
+
+            // Handle the ApplicationExit event to know when the application is exiting.
+            Application.ApplicationExit += new EventHandler(this.OnApplicationExit);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == MYACTION_HOTKEY_F4)
+            {
+                // My hotkey has been typed
+                button1.PerformClick();
+            }
+            else if (m.Msg == 0x0312 && m.WParam.ToInt32() == MYACTION_HOTKEY_F5)
+            {
+                // My hotkey has been typed
+                button5.PerformClick();
+            }
+            else if (m.Msg == 0x0312 && m.WParam.ToInt32() == MYACTION_HOTKEY_F6)
+            {
+                // My hotkey has been typed
+                button4.PerformClick();
+            }
+            else if (m.Msg == 0x0312 && m.WParam.ToInt32() == MYACTION_HOTKEY_F7)
+            {
+                // My hotkey has been typed
+                button7.PerformClick();
+            }
+            base.WndProc(ref m);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            richTextBox1.Text = System.IO.File.ReadAllText(@file_text);
+            richTextBox1.SelectionStart = int.Parse(System.IO.File.ReadAllText(@file_sel_pos));
+
+            startread = richTextBox1.SelectionStart;
+
+            richTextBox1.ScrollToCaret();
+            
+            // Progress Bar
+            progressBar1.Value = (int)Math.Round(Convert.ToDecimal(100*richTextBox1.SelectionStart/richTextBox1.TextLength), MidpointRounding.AwayFromZero);
+            label2.Text = richTextBox1.SelectionStart.ToString() +　"/" + richTextBox1.TextLength.ToString();
+        }
+
+        private void OnApplicationExit(object sender, EventArgs e)
+        {
+            // When the application is exiting...
+            System.IO.File.WriteAllText(@file_sel_pos, startread.ToString());
+            System.IO.File.WriteAllText(@file_text, richtextboxtext);
+            // UnregisterHotKey(this.Handle, MYACTION_HOTKEY_F4);
+            // UnregisterHotKey(this.Handle, MYACTION_HOTKEY_F6);
+        }
+
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richtextboxtext = richTextBox1.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // COPY OR EXPERIMENTAL BUTTOM
+            //Clipboard.SetText(richTextBox1.SelectedText);
+
+            // SEND TEXT Button
+            // Not being used for now. Maybe in the future
+            // FindWindowEx & SendMessage method.
+            /*
+            Process[] notepads = Process.GetProcessesByName("aegisub32");
+            if (notepads.Length == 0)
+            {
+                MessageBox.Show("windows not found");
+                return;
+            }
+            if (notepads[0] != null)
+            {
+                IntPtr child = FindWindowEx(notepads[0].MainWindowHandle, new IntPtr(0), "stcwindow", null);
+                System.Text.StringBuilder sb = new System.Text.StringBuilder(255);
+                SendMessage(new IntPtr(0x00E812E8), 0x0D, 11, sb);
+                // SendMessage(new IntPtr(0x00301258), 0x0C, 0, sb);
+
+                // SEND {ENTER}
+                // Use SetForegroundWindow & SendKeys instead of FindWindowEx & SendMessage
+                if (processname == null)
+                {
+                    MessageBox.Show("No Target Window Selected");
+                    return;
+                }
+                Process p = Process.GetProcessesByName(processname).FirstOrDefault();
+                if (p != null)
+                {
+                    // send {ENTER}
+                    IntPtr h = p.MainWindowHandle;
+                    
+                    SetForegroundWindow(h);
+                    SendKeys.SendWait("^4");
+                    // SendKeys.SendWait(sb.ToString());
+                }
+
+            }*/
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Process p = Process.GetProcessesByName(processname).FirstOrDefault();
+            if (p != null)
+            {
+                // send {ENTER}
+                IntPtr h = p.MainWindowHandle;
+
+                SetForegroundWindow(h);
+                SendKeys.SendWait("^4");
+                SendKeys.SendWait("{ENTER}");
+                SendKeys.SendWait("^p");
+                if (checkBox2.Checked)  // Send ctrl-s save command
+                {
+                    SendKeys.SendWait("^s");
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            startread = richTextBox1.SelectionStart;
+            System.IO.File.WriteAllText(@file_sel_pos, startread.ToString());
+            System.IO.File.WriteAllText(@file_text, richtextboxtext);
+            MessageBox.Show("Content and Position saved! ");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // SEND CTRL-S Save command
+            // Use SetForegroundWindow & SendKeys instead of FindWindowEx & SendMessage
+            if (processname == null)
+            {
+                MessageBox.Show("No Target Window Selected");
+                return;
+            }
+            Process p = Process.GetProcessesByName(processname).FirstOrDefault();
+            if (p != null)
+            {
+                // send {ENTER}
+                IntPtr h = p.MainWindowHandle;
+                SetForegroundWindow(h);
+                SendKeys.SendWait("^s");
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            processname = comboBox1.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // SEND {ENTER}
+            // Use SetForegroundWindow & SendKeys instead of FindWindowEx & SendMessage
+            if (processname == null)
+            {
+                MessageBox.Show("No Target Window Selected");
+                return;
+            }
+            Process p = Process.GetProcessesByName(processname).FirstOrDefault();
+            if (p != null)
+            {
+                // send {ENTER}
+                IntPtr h = p.MainWindowHandle;
+                SetForegroundWindow(h);
+                SendKeys.SendWait("{Enter}");
+            }
+
+
+                // SEND TEXT Button
+                // Not being used for now. Maybe in the future
+                // FindWindowEx & SendMessage method.
+                /*
+                Process[] notepads = Process.GetProcessesByName("aegisub32");
+                if (notepads.Length == 0)
+                {
+                    MessageBox.Show("windows not found");
+                    return;
+                }
+                if (notepads[0] != null)
+                {
+                    IntPtr child = FindWindowEx(notepads[0].MainWindowHandle, new IntPtr(0), "stcwindow", null);
+                    SendMessage(child, 0x000C, 0, richTextBox1.SelectedText);
+
+                }*/
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Previous Button
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Next Button
+
+            // check if cursor already reached the end.
+            if (richTextBox1.SelectionStart == richTextBox1.TextLength)
+            {
+                MessageBox.Show("Done! End of text reached!!!");
+                return;
+            }
+
+            // Use SetForegroundWindow & SendKeys instead of FindWindowEx & SendMessage
+            if (processname == null)
+            {
+                MessageBox.Show("No Target Window Selected");
+                return;
+            }
+            Process p = Process.GetProcessesByName(processname).FirstOrDefault();
+            if (p != null)
+            {
+                // get selected text
+                startread = richTextBox1.SelectionStart;
+                endread = startread;
+
+                while (endread < richTextBox1.TextLength &&
+                    !stopset.Contains(richTextBox1.Text[endread].ToString()))
+                {
+                    endread++;
+                }
+                while (endread+1 < richTextBox1.TextLength &&
+                     stopset.Contains(richTextBox1.Text[endread+1].ToString()))
+                {
+                    endread++;
+                }
+                // remove previous highlight
+                richTextBox1.Select(prevstartread, prevendread - prevstartread + 1);
+                richTextBox1.SelectionBackColor = Color.White;
+                richTextBox1.SelectionColor = Color.Black;
+
+                // highlight selected text
+                richTextBox1.Select(startread, endread - startread + 1);
+                richTextBox1.SelectionBackColor = Color.Blue;
+                richTextBox1.SelectionColor = Color.White;
+                prevstartread = startread;
+                prevendread = endread;
+
+                // copy text to clipboard
+                Clipboard.SetText(richTextBox1.SelectedText);
+
+                // send keys
+                IntPtr h = p.MainWindowHandle;
+                SetForegroundWindow(h);
+                SendKeys.SendWait(richTextBox1.SelectedText);
+                if (checkBox1.Checked)  // Send ENTER
+                {
+                    SendKeys.SendWait("{Enter}");
+                }
+                richTextBox1.ScrollToCaret();
+                endread++;
+                richTextBox1.SelectionStart = endread;
+                while (endread < richTextBox1.TextLength &&
+                    richTextBox1.Text[richTextBox1.SelectionStart].ToString() == "\n")
+                {
+                    richTextBox1.SelectionStart = richTextBox1.SelectionStart + 1;
+                }
+                startread = richTextBox1.SelectionStart;
+                progressBar1.Value = (int)Math.Round(Convert.ToDecimal(100 * richTextBox1.SelectionStart / richTextBox1.TextLength), MidpointRounding.AwayFromZero);
+                label2.Text = richTextBox1.SelectionStart.ToString() + "/" + richTextBox1.TextLength.ToString();
+            }
+        }
+    }
+}
